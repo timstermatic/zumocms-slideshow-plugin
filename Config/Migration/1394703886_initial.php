907cbd084c82b8e3a931d02a09052f8f0c0a943a
Tim Roberts <timstermatic@gmail.com>
2014-03-13 10:45:08 +0100

    adds migration and readme

---
+++ Config/Migration/1394703886_initial.php
@@ -0,0 +1,70 @@
+<?php
+class Initial extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'slides' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+					'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'content' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'position' => array('type' => 'integer', 'null' => false, 'default' => null),
+					'slideshow_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'slideshows' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+					'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'slides', 'slideshows'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-zumocms-slideshow-plugin
-========================
+zumocms-plugin-slideshow
+===================
+
+Slideshow plugin for zumocms - https://github.com/timstermatic/zumocms
+
+## Installation
+
+Clone into the Plugin directory of your zumocms installation as Slideshows`. For example from your app directory run:
+
+    git clone https://github.com/timstermatic/zumocms-plugin-slideshows Plugin/Slideshows
+    
+Run the database initialisation:
+  
+    ./Console/cake Migrations.migration run all --plugin Slideshows
+    
+    
+
